b0d4c27cdfc7aca4252c1526208e2832012d2930
Udayan Ghosh <udayanghosh@Udayan-Desktop-New>
2022-09-25 11:12:31 +0530

    quiz 2

--- plot_digits_classification.py
+++ plot_digits_classification.py
@@ -1,36 +1,14 @@
-"""
-================================
-Recognizing hand-written digits
-================================
 
-This example shows how scikit-learn can be used to recognize images of
-hand-written digits, from 0-9.
-
-"""
-
-# Author: Gael Varoquaux <gael dot varoquaux at normalesup dot org>
-# License: BSD 3 clause
-
-# Standard scientific Python imports
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from itertools import product as pdt
 
 # Import datasets, classifiers and performance metrics
 from sklearn import datasets, svm, metrics
 from sklearn.model_selection import train_test_split
+from skimage.transform import resize
 
-###############################################################################
-# Digits dataset
-# --------------
-#
-# The digits dataset consists of 8x8
-# pixel images of digits. The ``images`` attribute of the dataset stores
-# 8x8 arrays of grayscale values for each image. We will use these arrays to
-# visualize the first 4 images. The ``target`` attribute of the dataset stores
-# the digit each image represents and this is included in the title of the 4
-# plots below.
-#
-# Note: if we were working from image files (e.g., 'png' files), we would load
-# them using :func:`matplotlib.pyplot.imread`.
 
 digits = datasets.load_digits()
 
@@ -40,65 +18,137 @@ for ax, image, label in zip(axes, digits.images, digits.target):
     ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
     ax.set_title("Training: %i" % label)
 
-###############################################################################
-# Classification
-# --------------
-#
-# To apply a classifier on this data, we need to flatten the images, turning
-# each 2-D array of grayscale values from shape ``(8, 8)`` into shape
-# ``(64,)``. Subsequently, the entire dataset will be of shape
-# ``(n_samples, n_features)``, where ``n_samples`` is the number of images and
-# ``n_features`` is the total number of pixels in each image.
-#
-# We can then split the data into train and test subsets and fit a support
-# vector classifier on the train samples. The fitted classifier can
-# subsequently be used to predict the value of the digit for the samples
-# in the test subset.
+
 
 # flatten the images
 n_samples = len(digits.images)
-data = digits.images.reshape((n_samples, -1))
+X = digits.images.reshape((n_samples, -1))
+Y = digits.target
 
+test_frac=0.1
+dev_frac=0.1
+
+def get_train_dev_test_dataset(X, Y, dev_fract, test_fract):
+    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, shuffle=True)
+    x_train, x_dev, y_train, y_dev = train_test_split(x_train, y_train, test_size=0.2, shuffle=True)
+    return x_train, x_dev, x_test, y_train, y_dev, y_test 
+
+# model hyperparams
+GAMMA = [0.0001, 0.0004, 0.0005, 0.0008, 0.001]
+C = [0.5, 2.0, 3.0, 4.0, 5.0]
 # Create a classifier: a support vector classifier
-clf = svm.SVC(gamma=0.001)
+clf = svm.SVC()
 
-# Split data into 50% train and 50% test subsets
-X_train, X_test, y_train, y_test = train_test_split(
-    data, digits.target, test_size=0.5, shuffle=False
-)
+X_train, X_dev, X_test, y_train, y_dev, y_test = get_train_dev_test_dataset(X, Y, dev_frac, test_frac)
+gamma_=[]
+c_=[]
+accuracy=[]
+train_accuracy=[]
+dev_accuracy=[]
+test_accuracy=[]
+
+# Creating hyperparameters combinations
+samples=pdt(GAMMA,C)
+
+# iterating over each hyperparameter combinations
+for Gamma,c in samples:
+
+#PART: setting up hyperparameter
+    hyper_params = {'gamma':Gamma, 'C':c}
+    clf.set_params(**hyper_params)
+
 
 # Learn the digits on the train subset
-clf.fit(X_train, y_train)
+    clf.fit(X_train, y_train)
 
-# Predict the value of the digit on the test subset
-predicted = clf.predict(X_test)
+# Predict the value of the digit on the train, dev, test datasets
+    # Getting training accuracy
+    train_prediction = clf.predict(X_train)
+    train_accuracy.append(metrics.accuracy_score(y_train, train_prediction))
 
-###############################################################################
-# Below we visualize the first 4 test samples and show their predicted
-# digit value in the title.
+    # Getting dev accuracy
+    dev_prediction = clf.predict(X_dev)
+    #print(f"Classification report for classifier {clf}:\n"f"{metrics.classification_report(y_dev, prediction)}\n")
+    dev_accuracy.append(metrics.accuracy_score(y_dev,dev_prediction))
 
-_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-for ax, image, prediction in zip(axes, X_test, predicted):
-    ax.set_axis_off()
-    image = image.reshape(8, 8)
-    ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-    ax.set_title(f"Prediction: {prediction}")
+    # Getting test accuracy
+    test_prediction = clf.predict(X_test)
+    test_accuracy.append(metrics.accuracy_score(y_test, test_prediction))
+    gamma_.append(Gamma)
+    c_.append(c)
+gamma_ = np.array(gamma_)
+c_ = np.array(c_)
+train_accuracy = np.array(train_accuracy)
+dev_accuracy = np.array(dev_accuracy)
+test_accuracy = np.array(test_accuracy)
+combine=np.vstack((gamma_, c_, train_accuracy, dev_accuracy, test_accuracy))
+combine=combine.T
+column_values = ['GAMMA', 'C', 'Train Accuracy', 'Dev Accuracy', 'Test Accuracy']
+df = pd.DataFrame(data = combine, columns=column_values)
 
-###############################################################################
-# :func:`~sklearn.metrics.classification_report` builds a text report showing
-# the main classification metrics.
 
-print(
-    f"Classification report for classifier {clf}:\n"
-    f"{metrics.classification_report(y_test, predicted)}\n"
-)
+# Printing Train, Dev, Test accuracy with each combination of hyperparamameters
+print("Accuracy and hyperparameter matrix")
+print(df)
 
-###############################################################################
-# We can also plot a :ref:`confusion matrix <confusion_matrix>` of the
-# true digit values and the predicted digit values.
+# considering hyperparameters as best on which we're getting best accuracy on dev dataset
+i= dev_accuracy.argmax()
 
-disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
-disp.figure_.suptitle("Confusion Matrix")
-print(f"Confusion matrix:\n{disp.confusion_matrix}")
+#Printing Best accuracy
+print("Best accuracy we can find with parameters\n", "Gamma=",gamma_[i], "C=", c_[i],'\n', "Train Accuracy=%.2f" %(train_accuracy[i]*100), '%', 
+"Dev Accuracy=%.2f" %(dev_accuracy[i]*100), '%',"Test Accuracy=%.2f" %(test_accuracy[i]*100), '%' )
 
-plt.show()
+print("\n\n\n\n\n\n")
+
+test_frac_=0.2
+dev_frac_=0.2
+x_train, x_dev, x_test, y_train, y_dev, y_test = get_train_dev_test_dataset(X, Y, dev_frac_, test_frac_)
+
+for Gamma,c in samples:
+
+#PART: setting up hyperparameter
+    hyper_params = {'gamma':Gamma, 'C':c}
+    clf.set_params(**hyper_params)
+
+
+# Learn the digits on the train subset
+    clf.fit(x_train, y_train)
+
+# Predict the value of the digit on the train, dev, test datasets
+    # Getting training accuracy
+    train_prediction = clf.predict(x_train)
+    train_accuracy.append(metrics.accuracy_score(y_train, train_prediction))
+
+    # Getting dev accuracy
+    dev_prediction = clf.predict(x_dev)
+    #print(f"Classification report for classifier {clf}:\n"f"{metrics.classification_report(y_dev, prediction)}\n")
+    dev_accuracy.append(metrics.accuracy_score(y_dev,dev_prediction))
+
+    # Getting test accuracy
+    test_prediction = clf.predict(x_test)
+    test_accuracy.append(metrics.accuracy_score(y_test, test_prediction))
+    gamma_.append(Gamma)
+    c_.append(c)
+gamma_ = np.array(gamma_)
+c_ = np.array(c_)
+train_accuracy = np.array(train_accuracy)
+dev_accuracy = np.array(dev_accuracy)
+test_accuracy = np.array(test_accuracy)
+combine=np.vstack((gamma_, c_, train_accuracy, dev_accuracy, test_accuracy))
+combine=combine.T
+column_values = ['GAMMA', 'C', 'Train Accuracy', 'Dev Accuracy', 'Test Accuracy']
+df = pd.DataFrame(data = combine, columns=column_values)
+
+
+# Printing Train, Dev, Test accuracy with each combination of hyperparamameters
+print("Accuracy and hyperparameter matrix with different train dev test split")
+print(df)
+
+# considering hyperparameters as best on which we're getting best accuracy on dev dataset
+i= dev_accuracy.argmax()
+
+#Printing Best accuracy
+print("Best accuracy we can find with parameters with different train dev test split\n", "Gamma=",gamma_[i], "C=", c_[i],'\n', "Train Accuracy=%.2f" %(train_accuracy[i]*100), '%', 
+"Dev Accuracy=%.2f" %(dev_accuracy[i]*100), '%',"Test Accuracy=%.2f" %(test_accuracy[i]*100), '%' )
+
+print("\n\n\n\n\n\n")
